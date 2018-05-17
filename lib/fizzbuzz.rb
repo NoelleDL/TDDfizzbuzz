853946eee27949c0b2af88988f4df01039511731
NoelleDL <noelle.legrain@gmail.com>
2018-05-17 13:45:19 +0100

    refactored to single line

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,3 @@
 def fizzbuzz(number)
-  return 'fizzbuzz' if (number % 15).zero?
-  return 'fizz' if (number % 3).zero?
-  return 'buzz' if (number % 5).zero?
-  number
+  (number % 15).zero? ? 'fizzbuzz' : (number % 3).zero? ? 'fizz' : (number % 5).zero? ? 'buzz' : number
 end
