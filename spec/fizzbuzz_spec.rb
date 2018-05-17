0d0c1283a888b39859d8d9d5b755309c2d53c2e7
NoelleDL <noelle.legrain@gmail.com>
2018-05-17 13:34:11 +0100

    Added two more tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,5 +7,13 @@ describe '#fizzbuzz' do
 
   it "return 'buzz' when given multiple of 5" do
     expect(fizzbuzz(5)).to eq 'buzz'
-  end 
+  end
+
+  it "return 'fizzbuzz' when given multiple of 3 & 5" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+  it "returns number when not a multiple of 3 or 5" do
+    expect(fizzbuzz(2)).to eq 2
+  end
 end
